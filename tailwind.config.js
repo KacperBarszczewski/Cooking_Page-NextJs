334bc6f90fcfba364bb23078e68d116aa7ffaa49
KacperB <kacpero.barszczewski@gmail.com>
2023-04-13 15:22:32 +0200

    config: add color palett to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#ea7714',
+        'error': '#c44569',
+        'foreground': '#484748',
+        'background': {
+          DEFAULT: '#ffffff',
+          dark: '#282728',
+        },
+        'typography': {
+          DEFAULT: '#000000',
+          dark: '#c8d6e5',
+        },
+      },
+    },
   },
   plugins: [],
 }
