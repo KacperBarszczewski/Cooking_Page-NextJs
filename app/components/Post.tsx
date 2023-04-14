0b21580089065af1f2fb03ecd8301405adc2e91a
KacperB <kacpero.barszczewski@gmail.com>
2023-04-14 22:37:58 +0200

    style: add hover effect on main page

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -12,11 +12,13 @@ type Props = {
 
 export default function Post({ id, postTitle, comments, image }: Props) {
     return (
-        <Link href={`/post/${id}`} className="p-6 block w-[20rem]">
-            <div>
+        <Link href={`/post/${id}`} className="p-6 block w-[20rem] rounded-lg hover:shadow-primary hover:shadow-lg group  ">
+            <div className="relative">
                 {
-                    image ? <Image className="object-cover h-36 w-[20rem] rounded" width={200} height={200} src={image} alt={postTitle + " image"} /> : <div className="w-52 h-52" />
+                    image ? <Image className="object-cover h-36 w-[17rem] rounded-lg" width={200} height={200} src={image} alt={postTitle + " image"} /> : <div className="h-36 w-[17rem] flex justify-center items-center"> No Image</div>
+
                 }
+                <div className="absolute bottom-0 left-0 right-0 h-36 w-[17rem] rounded-lg group-hover:shadow-[inset_30px_0px_25px_#E58A2F,inset_-30px_0px_25px_#E58A2F]"></div>
             </div>
             <div>
                 <h2 className="font-semibold">{postTitle}</h2>
